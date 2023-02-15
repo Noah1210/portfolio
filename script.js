e51be7f25243c49d5e467eba941b886152f88588
Noah Pardon <noah.pardon@mboadigital.tech>
2023-02-15 23:45:46 +0100

    Added smooth scrolling

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Creating a menu
+
 let menu = document.querySelector('#menu-icon');
 let nav__links = document.querySelector('.nav__links');
 
@@ -6,7 +8,23 @@ menu.onclick = () => {
     nav__links.classList.toggle('open');
 }
 
+// Determining which section is active
 
+const li = document.querySelectorAll(".links")
+const sec= document.querySelectorAll("section")
+
+function activeMenu(){
+    let len = sec.length;
+    while(--len && window.scrollY + 97 < sec[len].offsetTop){}
+    li.forEach(ltx => ltx.classList.remove("active"));
+    li[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll", activeMenu);
+
+
+// Changing the display on button click 
 
 let school = document.querySelector('.school')
 let work = document.querySelector('.work')
